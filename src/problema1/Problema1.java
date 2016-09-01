8484aceced4d3a976c929a719992532220a9965a
Portatil <Portatil@Portatil-PC.home>
2016-08-31 22:33:30 -0500

    First Commit

---
+++ src/problema1/Problema1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema1;
+
+/**
+ *
+ * @author Portatil
+ */
+public class Problema1 {
+    public static void main(String[] args) {
+        System.out.println("Primera Funcion en Main");
+        func1();
+        func2();
+        System.out.println("Se Termina Main");
+    }
+    
+    public static void func1(){
+        System.out.println("Segunda Funcion");
+    }
+    
+    public static void func2(){
+        System.out.println("Tercera Funcion");
+    }
+}
+
